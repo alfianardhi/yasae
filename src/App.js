6b4126b00e2857de804c4b930938b52765ce64d8
alfianardhi <ardhi.alfian@gmail.com>
2021-05-11 01:42:27 +0700

    inprogrs header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import './assets/css/app.css'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import Homepage from 'pages/HomePage'
+
 function App() {
-  return <div className='App'>hi</div>
+  return (
+    <div className='App'>
+      <Router>
+        <Route path='/' exact component={Homepage} />
+      </Router>
+    </div>
+  )
 }
 
 export default App
---
+++ src/pages/HomePage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Header from 'parts/Header'
+
+export default function HomePage(props) {
+  return (
+    <>
+      <Header></Header>
+    </>
+  )
+}
