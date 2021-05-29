c3d31711feed4711ec719eb4f8925b6c52f028f2
alfianardhi <ardhi.alfian@gmail.com>
2021-05-29 23:23:11 +0700

    inprog card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -8,15 +8,15 @@ export default function Card(props) {
     <div className='px-3 w-2/4 mb-4 md:w-3/12 xl:w-2/12'>
       <div className='rounded-md p-4 pb-4 relative bg-white shadow'>
         <div className='rounded-md overflow-hidden card-shadow product-thumb w-full h-48'>
-          <img className='w-full h-full object-cover object-center' src='/images/content/mustard-green.png' alt='product 1' />
-          <span className='absolute top-5 left-4 px-2 py-1 bg-orange-400 text-white text-xs rounded-sm block'>30%</span>
+          <img className='w-full h-full object-cover object-center' src={image} alt={title} />
+          <span className='absolute top-5 left-4 px-2 py-1 bg-orange-400 text-white text-xs rounded-sm block'>{`${discount}%`}</span>
         </div>
         <div className='product-content w-full'>
-          <span className='cata-product text-xs text-gray-400'>Baby Pak Choy Conven</span>
-          <h6 className='product-title font-medium text-base mt-1.5'>Baby Pak Choy</h6>
-          <p className='quantity'>250 gram</p>
+          <span className='cata-product text-xs text-gray-400'>{description}</span>
+          <h6 className='product-title font-medium text-base mt-1.5'>{title}</h6>
+          <p className='quantity'>{`${unit} gram`}</p>
           <div className='price text-base text-green-500 mt-0.5'>
-            $2.00 <del>$5.00</del>
+            {`Rp ${price.toLocaleString('id')}`} <del>{`Rp ${actualPrice.toLocaleString('id')}`}</del>
           </div>
         </div>
         <div className='product-button mt-4 w-full'>
