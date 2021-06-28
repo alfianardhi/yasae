fc0b0e3b17054d1dd1bb6c44f89e710a0e57f0d7
alfianardhi <ardhi.alfian@gmail.com>
2021-06-28 23:41:04 +0700

    update

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 )
+// TODO: dev state, pages
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
